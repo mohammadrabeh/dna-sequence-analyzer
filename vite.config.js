b9c1b1052ed1a00682bb582de944dde0fd61f1c9
mohammadrabeh <1996media8@gmail.com>
2025-11-26 08:22:10 +0300

    Remove build configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dna-sequence-analyzer/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: '/dna-sequence-analyzer/'
 })
